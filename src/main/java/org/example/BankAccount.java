e79fd9f037d2e281be924799c24f82cc1ceb102b
Barry <barryrichards05@gmail.com>
2026-02-27 10:31:57 +0000

    implemented deposit and withdraw methods

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -2,7 +2,20 @@ package org.example;
 
 public class BankAccount {
 
+    int balance;
+
     public static void main(String[] args) {
-        System.out.println("Hello from bank account");
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(20);
+        bankAccount.withdraw(10);
+
+    }
+
+    public void deposit(int amount){
+        this.balance += amount;
+    }
+
+    public void withdraw(int amount){
+        this.balance -= amount;
     }
 }
