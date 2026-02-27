3e4b5b42e5d9cfe28426b574d7bb728466e65c3a
Barry <barryrichards05@gmail.com>
2026-02-27 10:35:01 +0000

    added main class

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -4,13 +4,6 @@ public class BankAccount {
 
     int balance;
 
-    public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount();
-        bankAccount.deposit(20);
-        bankAccount.withdraw(10);
-
-    }
-
     public void deposit(int amount){
         this.balance += amount;
     }
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(20);
+        bankAccount.withdraw(10);
+    }
+
+}
